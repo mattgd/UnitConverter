4d5bdac171342b917e6f5de3b6bb6e61dbc4654b
Mridul Gupta <mridul15061@iiitd.ac.in>
2016-10-15 04:51:18 +0530

    Added converters of Force
    With reference to issue #17

--- unit_converter.py
+++ unit_converter.py
@@ -217,9 +217,49 @@ def psi_to_Torr(value):
 def n_to_dyn(newtons):
     return newtons * 10^5
 
+# Dynes to Newtons (n)
+def dyn_to_n(dynes):
+    return dynes * 0.00001 #Precision of 5 significant digits
+
 # Newtons to Poundals (pdl)
 def n_to_pdl(newtons):
     return newtons * 7.23301 #Precision of 5 significant digits
+
+# Poundals to Newtons (n)
+def pdl_to_n(poundals):
+    return poundals * 0.13826 #Precision of 5 significant digits
+
+# Newtons to Kilogram-Force (kp)
+def n_to_kp(newtons):
+    return newtons * 0.10197 #Precision of 5 significant digits
+
+# Kilogram-Force to Newtons
+def kp_to_n(kps):
+    return kps * 9.80665 #Precision of 5 significant digits
+
+# Dyne to Kilogram-Force (kp)
+def dyn_to_kp(dynes):
+    return n_to_kp(dyn_to_n(dynes))
+
+# Kilogram-Force to Dyne (dyn)
+def kp_to_dyn(kps):
+    return n_to_dyn(kp_to_n(kps))
+
+# Dynes to Poundals (pdl)
+def dyn_to_pdl(dynes):
+    return n_to_pdl(dyn_to_n(dynes))
+
+# Poundals to Dynes (dyn)
+def pdl_to_dyn(poundals):
+    return n_to_dyn(pdl_to_n(poundals))
+
+# Kilogram-Force to Poundals (pdl)
+def kp_to_pdl(kps):
+    return n_to_pdl(kp_to_n(kps))
+
+# Poundals to Kilogram-Force (kp)
+def pdl_to_kp(poundals):
+    return n_to_kp(pdl_to_n(poundals))
 
 # TIME CONVERSION
 def convert_time(value, units_from_base, units_to_base):
