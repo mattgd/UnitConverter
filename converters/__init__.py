d5095ca749b1ccdf3116b3b5b5fd15af4112354e
Oscar Ruckdeschel <plasmaosc@aol.com>
2016-10-21 00:57:54 +0200

    Issue #33: Implemented ways to get information from the UNITS array

--- converters/__init__.py
+++ converters/__init__.py
@@ -33,7 +33,8 @@ def convert(from_unit, to_unit, *args, **kwargs):
                 # remove already given variables
                 missing_si_varname.pop(0)
                 # only not given variable names are listed
-            if convert_function.func_defaults is not None and len(convert_function.func_defaults) == len(missing_si_varname):
+            if convert_function.func_defaults is not None and len(convert_function.func_defaults) == len(
+                    missing_si_varname):
                 # should not be >= because if all other params are optional
                 # it means that only one has to be set for the function to work (specification)
                 raise RequireAdditionalParamError(missing_si_varname)
@@ -80,6 +81,59 @@ def get_si(unit):
     return cat_unit["si"]
 
 
+def get_suggestions_for_category(category = None):
+    """
+    Returns categories currently accepted by the program.
+    :param category: Optional. If given: search all units for the given category.
+                        If not given give all categories.
+    :return:    Returns all categories accepted if no category was given.
+                If a category was given returns all units for that category.
+    """
+    if category is not None:
+        # a category was given -> all units for that category
+        category = _find_category(category)
+
+        if category is None:
+            return None
+
+        return [_format_unit_output(unit)
+                for unit in category["units"]]
+
+    # no category was given -> give all possible categories
+    return [category["name"]
+            for category in UNITS]
+
+
+def get_suggestions_to_given_unit(unit):
+    """
+    Lists all units that the given unit can be converted to
+    :param unit: The unit which should be converted
+    :return: All units that the given unit can be converted to
+    """
+    unit = _find_unit(unit)
+
+    if unit is None:
+        return None
+
+    return [_format_unit_output(single_unit)
+            for single_unit in _find_unit_category(unit["_internal_function_"])["units"]
+            if unit["si"] not in single_unit["_internal_accepted_names"]                    # the given unit should not be listed as a "convert to" unit
+            and "_internal_conversion" in single_unit                                       # catch lazy programmers faults (no _internal_conversion to one unit)
+            and unit["_internal_function_"] in single_unit["_internal_conversion"]]         # the given unit should be convertable to the single_unit
+
+
+def _find_category(category_name):
+    """
+    Internal: Load a category by name from the UNITS array
+    :param category_name: The category name to search the array to
+    :return: The category from the UNITS array or None if none was found
+    """
+    for category in UNITS:
+        if category_name.lower() == category["name"].lower():
+            return category
+    return None
+
+
 def _find_unit_category(unit):
     """
     Internal: Load the category of one unit from the UNITS array
@@ -106,3 +160,12 @@ def _find_unit(unit):
             if unit in cat_unit["_internal_accepted_names"] or unit == cat_unit["name"].lower():
                 return cat_unit
     return None
+
+
+def _format_unit_output(unit):
+    """
+    Formats an unit to get outputed by the system (Format: "<name> (<function_name to enter>)")
+    :param unit:
+    :return:
+    """
+    return unit["name"] + " (" + unit["_internal_function_"] + ")"
